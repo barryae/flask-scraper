aa14fa5807238810bd55ed9561fdf392651ebed1
Green Vine <barryaervin@gmail.com>
2020-05-16 14:32:31 -0400

    adding comments for clarity

--- flaskr/scrape.py
+++ flaskr/scrape.py
@@ -1,9 +1,10 @@
 import functools
+from flaskr.db import get_db
+from flaskr.webScraper import scrape
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from flaskr.db import get_db
-from flaskr.webScraper import scrape
+
 bp = Blueprint('', __name__, url_prefix='/')
 results = ''
 
@@ -11,33 +12,42 @@ results = ''
 @bp.route('/', methods=('GET', 'POST'))
 def search():
     if request.method == 'POST':
-        searchWord = request.form['searchWord']
-        analysisType = request.form['format']
-        print(analysisType)
         db = get_db()
         error = None
+
+        # Bring in parameters from form
+        searchWord = request.form['searchWord']
+        analysisType = request.form['format']
+
+        # Catching input errors
+        if not searchWord:
+            error = 'Please put in a word to scrape.'
+        elif not analysisType:
+            error = 'Analysis type not set.'
+
+        # Initialize with searched and analyzed being none
+        searchedAndAnalyzed = None
+
+        # Query to see if search_word exists
         alreadySearched = db.execute(
             'SELECT id, search_word FROM search WHERE search_word = ?', (
                 searchWord,)
         ).fetchone()
-        searchedAndAnalyzed = None
 
+        # If it has been searched, we then query the search_word and analysis type
+        #  to see if this particular analysis type has been saved
         if alreadySearched is not None:
             searchedAndAnalyzed = db.execute(
                 'SELECT id, body FROM analysis WHERE search_id = ? AND analysis_type = ?', (
                     alreadySearched['id'], analysisType)
             ).fetchone()
 
-        if not searchWord:
-            error = 'Please put in a word to scrape.'
-        elif not analysisType:
-            error = 'Analysis type not set.'
-
+        # If the word has been searched and been analyzed in the specified way, we return searchedAndAnalyzed
         if searchedAndAnalyzed is not None:
             results = searchedAndAnalyzed['body']
+            return renderResults(results, searchWord, analysisType)
 
-            return render(results, searchWord, analysisType)
-
+        # If it has not been searched before, we search and scrape, save the search word into search table
         if error is None:
             # scrape using code from web-scraper.py
             body = scrape(searchWord, analysisType)
@@ -62,13 +72,13 @@ def search():
 
             results = body
 
-            return render(results, searchWord, analysisType)
+            return renderResults(results, searchWord, analysisType)
 
         flash(error)
 
-    return render(results, searchWord, analysisType)
+    return renderResults(results, searchWord, analysisType)
 
 
-def render(results, searchWord, analysisType):
+def renderResults(results, searchWord, analysisType):
     return render_template('./scraper/scraper.html',
                            results=results, search=searchWord, analysisType=analysisType)
